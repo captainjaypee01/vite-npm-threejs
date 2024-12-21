71e94baa223aa72f6e9b391c0a470ec042bb4d06
John Paul Dala <jaypee@lingjack.com>
2024-12-21 17:20:26 +0800

    Add animation

--- src/main.js
+++ src/main.js
@@ -35,4 +35,28 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
-renderer.render(scene, camera);
+// 6. Add OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// 7. Add Animations
+function animate() {
+    requestAnimationFrame(animate);
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    box.rotation.y += 0.005;
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+// 8. Handle Window Resizing
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+animate()
